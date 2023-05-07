0d07ae438144113a33d6dc1aba1ccaa9ef0afc44
jaredchandler <jaredchandler@users.noreply.github.com>
2023-05-07 11:14:40 -0400

    Update config.sh

--- config.sh
+++ config.sh
@@ -7,6 +7,7 @@ EOF
 sudo systemctl daemon-reload
 sudo systemctl restart ssh
 ssh-keygen -t rsa -b 4096 -C "lihs@punkt.de" -f foo
+apt update
 apt install python3-pip -y
 pip3 install twisted
 pip3 install pyOpenSSL
